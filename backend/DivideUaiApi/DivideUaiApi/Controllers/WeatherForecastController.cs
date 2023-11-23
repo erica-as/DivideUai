65ba6946a5eb39d3619d9f4ada78d43bbb0e0ec1
erica-as <erica.asnt@hotmail.com>
2023-11-22 21:35:30 -0300

    estrutura backend api divideuai

---
+++ backend/DivideUaiApi/DivideUaiApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DivideUaiApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        
+    }
+}
